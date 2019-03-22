7b3262b859a54ca94384fdc991c796ef512dc2c8
Leo-Mun <vmfn0401@gmail.com>
2019-03-22 04:54:58 -0700

    add function to upload

--- dream/dream/backends/backend.py
+++ dream/dream/backends/backend.py
@@ -1,7 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api, reqparse
 
+import magic
+
 from dream.extract import pdf
+from dream.util import predict
 
 application = Flask(__name__)
 
@@ -12,13 +15,26 @@ def Print():
 
 
 # TODO(@LEO_MOON) Feature extraction based on file
-@application.route("/upload")
+@application.route("/upload", methods=["POST"])
 def upload_file():
     if request.method == "POST":
         f = request.files["file"]
 
+        file_magic = magic.from_buffer(f)
+
+        if "PDF" in file_magic:
+            feature = pdf.extract(f)
+
+            probability = predict.predict(feature, 0)
 
 
+        elif "Word" in file_magic:
+            pass
+
+        elif "Hangul" in file_magic:
+            pass
+
+        return jsonify({"probability", probability})
 
 
 if __name__ == '__main__':
