bf22a18dd4b27ae142ad018078e4462015f85a02
Leo-Mun <vmfn0401@gmail.com>
2019-03-22 04:54:16 -0700

    add main function

--- dream/dream/__main__.py
+++ dream/dream/__main__.py
@@ -2,12 +2,11 @@ import sys
 import os
 
 
-# TODO(@LEO_MOON): modify entry point
-def main():
-    from dream.cli import cli
+def run():
+    from .cli import cli
 
     cli.main()
 
 
 if __name__ == '__main__':
-    main()
+    run()
