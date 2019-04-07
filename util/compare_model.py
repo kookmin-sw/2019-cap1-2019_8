124a3d80cd6eec84b92ac5338b7842cbf3b64dc3
hyeri <kbi09@naver.com>
2019-04-07 23:41:27 +0900

    add lgb model

--- util/compare_model.py
+++ util/compare_model.py
@@ -7,8 +7,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import AdaBoostClassifier
+import lightgbm as lgb
 from sklearn.metrics import accuracy_score
 
 config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
@@ -35,6 +35,41 @@ def compare_model(x_train, y_train):
         print(name, "'s Accuracy is ", accuracy_score(y_train, y_pred))
 
 
+def run_lgb(x_train, y_train)
+    # create dataset for lightgbm
+    lgb_train = lgb.Dataset(x_train, y_train)
+    # specify your configurations as a dict
+    params = {
+        'boosting_type': 'gbdt',
+        'objective': 'regression',
+        'metric': {'l2', 'l1'},
+        'num_leaves': 31,
+        'learning_rate': 0.05,
+        'feature_fraction': 0.9,
+        'bagging_fraction': 0.8,
+        'bagging_freq': 5,
+        'verbose': 0
+    }
+    print('Starting training...')
+    # train
+    gbm = lgb.LGBMRegressor(num_leaves=31,
+                            learning_rate=0.05,
+                            n_estimators=20)
+    gbm.fit(x_train, y_train,
+            eval_set=[(x_train, y_train)],
+            eval_metric='l1',
+            early_stopping_rounds=10)
+    y_pred = gbm.predict(x_train)
+    # convert into binary values
+    for i in range(len(y_pred)):
+        if y_pred[i] >= .5:
+            y_pred[i] = 1
+        else:
+            y_pred[i] = 0
+
+    print("LGB's Accuracy is ", accuracy_score(y_train, y_pred))
+
+
 
 if __name__ == "__main__":
     df = pd.read_csv(data)
@@ -42,3 +77,4 @@ if __name__ == "__main__":
     y_train = df.iloc[:, -1].values
 
     compare_model(x_train, y_train)
+    run_lgb(x_train, y_train)
