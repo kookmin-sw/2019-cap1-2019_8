5cf2d98e6c18975b8bd061e43cb4ff1b0f000795
Leo-Mun <vmfn0401@gmail.com>
2019-04-17 23:40:54 -0700

    modify script

--- util/labeling.py
+++ util/labeling.py
@@ -1,51 +1,55 @@
-import json
 import os
-import csv
+import shutil
+import json
+import glob
+import magic
 
-_report_path = r''
-
-
-def walk_dir(input_path):
-    file_list = list()
-    for path, dirs, files in os.walk(input_path):
-        for file in files:
-            file_path = os.path.join(path, file)
-            file_list.append(file_path)
-    return file_list
+report_list = []
+for path, dirs, files in os.walk(VT_REPORT_PATH):
+    for file in files:
+        if file.split(".")[-1] == "json":
+            report_list.append(os.path.join(path, file))
 
 
-def file_parsing(report_list):
-    file_dict = {}
-
+print(len(report_list))
+with open(CSV_PATH, "w") as f:
+    f.write("file,label\n")
     for file in report_list:
-        file_name = os.path.basename(file).split('.')[0]
-
         with open(file, "r") as r:
             report = json.load(r)
+
         try:
-            if report["results"]["positives"] >= 10:
-                file_dict[file_name] = 1
+            file_hash = report["results"]["md5"]
+            label = -1
+
+            try:
+                f_sec = report["results"]["scans"]["F-Secure"]["detected"]
+            except:
+                f_sec = None
+
+            try:
+                kas = report["results"]["scans"]["Kaspersky"]["detected"]
+            except:
+                kas = None
+
+            try:
+                sym = report["results"]["scans"]["Symantec"]["detected"]
+            except:
+                sym = None
+
+            if (f_sec or kas or sym) is True:
+                label = 1
 
             elif report["results"]["positives"] == 0:
-                file_dict[file_name] = 0
+                label = 0
 
-        except Exception as ep:
-            print("error: ", ep)
+            if label > -1:
+                f.write(f"{file_hash},{label}\n")
 
-        finally:
+        except Exception as e:
+            print(e)
             pass
 
-    return file_dict
 
 
-def csv_write(report_result_set):
-    with open('output.csv', 'w', encoding='utf-8', newline='') as f:
-        for i in list(report_result_set.keys()):
-            wr = csv.writer(f)
-            wr.writerow([i, report_result_set[i]])
 
-
-if __name__ == '__main__':
-    r_list = walk_dir(_report_path)
-    f_dict = file_parsing(r_list)
-    csv_write(f_dict)
