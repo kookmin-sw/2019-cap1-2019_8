c8d4d4d867951b92c9706e259ae42352567013c1
Leo-Mun <vmfn0401@gmail.com>
2019-05-14 20:29:26 -0700

    add packages

--- dreamav/setup.py
+++ dreamav/setup.py
@@ -1,4 +1,8 @@
+import glob
+import os
 from setuptools import setup, find_packages
+
+from dreamav.__init__ import __ROOT__
 
 def get_requirements():
     ret = []
@@ -10,12 +14,20 @@ def get_requirements():
 
 
 requires = get_requirements()
-
 setup(
     name = 'dreamav',
-    version = '0.2.2',
-    # packages = ['dreamav'],
+    version = '0.2.5',
+    description = "This package is for detecting malicious document",
+    author = ["Damin Moon", "Kihwan Kim", "HyunSeok Kim", "Hyeri Jung", "Yuhan Bang"],
+    author_email = "vmfn0401@gmail.com",
+    license = "MIT",
+    url = "https://github.com/kookmin-sw/2019-cap1-2019_8",
     packages=find_packages(),
+    package_data = {
+        "model": glob.glob(os.path.join(__ROOT__, "util/model/*")),
+        "ini": [os.path.join(__ROOT__, "dream.ini")]
+    },
+    zip_safe=False,
     setup_requires=requires,
     entry_points = {
         'console_scripts': [
